6ae53b6f03eb472d065481e075c2393cac6cdf8a
vinicius <vinicius.sousa5@icloud.com>
2025-09-19 08:25:04 -0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,14 @@
+function getWelcomeMessage(message) {
+    alert(message);
+}
+getWelcomeMessage("Hello, World!");
+
+function divideNum (a, b) {
+    return a / b;
+}
+console.log(divideNum(10, 2));
+
+function multiplyNum (a, b) {
+    return a * b;
+}
+console.log(multiplyNum(10, 2));
